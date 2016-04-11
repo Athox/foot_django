0c3bdf4786dd8f987ecc25b354f99fe2aa4ca280
Anthony <cot.anthony@gmail.com>
2016-04-11 21:15:25 +0200

    a

--- foot_django/urls.py
+++ foot_django/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from site import views
+from blog import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^mama', views.papa),
+    url(r'^accueil$', views.home),
 ]
