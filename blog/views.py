6af5901d72b937917802c29c92f5e9d7f9c39f16
Anthony <cot.anthony@gmail.com>
2016-04-11 23:18:00 +0200

    a

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,5 @@ from blog.models import Championnat
 
 
 def liste_pays(request):
-    pays = Championnat.objects.only("pays_championnat").order_by("pays_championnat")
+    pays = Championnat.objects.all().order_by("pays_championnat")
     return render(request, 'liste_pays.html', {'pays': pays})
